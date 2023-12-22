988c3351ea8937be12437cfa6496802a82056f93
majid mohammadian <majeedmohammadian@gmail.com>
2023-12-22 15:01:46 +0330

    add config file

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data setting
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("SETTING_CACHE_TIME", 0),
+
+];
