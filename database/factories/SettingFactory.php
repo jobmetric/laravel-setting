0b4a3ee39b597f31de0e7faf8efa28320f56dbea
majid mohammadian <majeedmohammadian@gmail.com>
2023-12-22 15:02:49 +0330

    add model and factory

---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace JobMetric\Setting\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use JobMetric\Setting\Models\Setting;
+
+/**
+ * @extends Factory<Setting>
+ */
+class SettingFactory extends Factory
+{
+    protected $model = Setting::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => $this->faker->word,
+            'key' => $this->faker->word,
+            'value' => $this->faker->word,
+            'is_json' => false,
+        ];
+    }
+
+    /**
+     * set code
+     *
+     * @param string $code
+     *
+     * @return static
+     */
+    public function setCode(string $code): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'code' => $code
+        ]);
+    }
+
+    /**
+     * set key
+     *
+     * @param string $key
+     *
+     * @return static
+     */
+    public function setKey(string $key): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'key' => $key
+        ]);
+    }
+
+    /**
+     * set value
+     *
+     * @param string $value
+     *
+     * @return static
+     */
+    public function setValue(string $value): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'value' => $value
+        ]);
+    }
+
+    /**
+     * set is json
+     *
+     * @param bool $is
+     *
+     * @return static
+     */
+    public function setIsJson(bool $is): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'is_json' => $is
+        ]);
+    }
+}
