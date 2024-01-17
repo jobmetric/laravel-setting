fb3952859c5bb426d0854a98795e516569543f68
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-17 23:37:36 +0330

    load setting in global object

--- src/SettingServiceProvider.php
+++ src/SettingServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace JobMetric\Setting;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Cache;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
+use JobMetric\Setting\Models\Setting;
+use JobMetric\Setting\Setting as SettingService;
 
 class SettingServiceProvider extends PackageCoreServiceProvider
 {
@@ -22,5 +26,28 @@ class SettingServiceProvider extends PackageCoreServiceProvider
             ->hasConfig()
             ->hasMigration()
             ->registerClass('Setting', Setting::class);
+    }
+
+    /**
+     * after boot package
+     *
+     * @return void
+     */
+    public function afterBootPackage(): void
+    {
+        $settings = Cache::remember('setting', config('setting.cache_time'), function () {
+            $data = [];
+            if(Schema::hasTable(config('setting.tables.setting'))) {
+                $results = Setting::all();
+
+                foreach ($results as $setting) {
+                    $data[$setting->code.'_'.$setting->key] = ($setting->is_json) ? json_decode($setting->value, true) : $setting->value;
+                }
+            }
+
+            return $data;
+        });
+
+        app(SettingService::class)->setAll($settings);
     }
 }
