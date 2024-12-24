7deafb3f4d2fa4fa839a8ad0d6f2cce4fd359678
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-24 23:36:04 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Setting Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Setting for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "messages" => [
+        "saved" => ":type created successfully.",
+    ],
+
+];
