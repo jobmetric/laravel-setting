ab151952678502ea09792667afb0872f47218fab
majidmohammadian <majeedmohammadian@gmail.com>
2026-01-18 02:35:11 +0330

    added test case file

---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JobMetric\Setting\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use JobMetric\Setting\SettingServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    use RefreshDatabase;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            SettingServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        loadMigrationPath(__DIR__ . '/database/migrations');
+    }
+}
